d37cca9ea37950b22fa6f7dddbcfbd17bbb15664
yeony1011 <yeony1011@gmail.com>
2025-04-05 13:35:34 +0900

    add : day09, day10

---
+++ day09/src/App.js
@@ -0,0 +1,48 @@
+import TabBar from './components/TabBar.js';
+import Content from './components/Content.js';
+
+import { request } from './components/api.js';
+
+export default function App($app) {
+    this.state = {
+        currentTab: 'all',
+        photos: [],
+    };
+
+    //tab
+    const tab = new TabBar({
+        $app,
+        initialState: '',
+        onClick: async (name) => {
+            this.setState({
+                ...this.state,
+                currentTab: name,
+                photos: await request(name === 'all' ? '' : name),
+            });
+        },
+    });
+
+    //content
+    const content = new Content({ $app, initialState: [] });
+
+    //state
+    this.setState = (newState) => {
+        this.state = newState;
+        tab.setState(this.state.currentTab);
+        content.setState(this.state.photos);
+    };
+
+    const init = async () => {
+        try {
+            const initialPhotos = await request();
+            this.setState({
+                ...this.state,
+                photos: initialPhotos,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    init();
+}
