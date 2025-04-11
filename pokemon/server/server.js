cd14597529f8d5dcd8ad1de9bd73177210bd485f
yeony1011 <yeony1011@gmail.com>
2025-04-11 17:39:11 +0900

    update : pokemon server.js 수정

--- pokemon/server/server.js
+++ pokemon/server/server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '..')));
 
-app.get('/*', (req, res) => {
+app.get('/{*any}', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
